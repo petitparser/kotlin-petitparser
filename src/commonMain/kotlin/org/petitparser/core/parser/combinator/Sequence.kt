21397dce44d91fb45fa62f3e64fb7855b7aa8d59
Lukas Renggli <renggli@gmail.com>
2022-09-26 23:53:29 +0200

    Fix typing of sequence parser.

--- src/commonMain/kotlin/org/petitparser/core/parser/combinator/Sequence.kt
+++ src/commonMain/kotlin/org/petitparser/core/parser/combinator/Sequence.kt
@@ -9,12 +9,12 @@ import org.petitparser.core.parser.Parser
 fun <R> seq(vararg parsers: Parser<R>): Parser<List<R>> = SequenceParser(listOf(*parsers))
 
 /** Returns the sequence of this parser followed by [other]. */
-infix operator fun <R> Parser<R>.plus(other: Parser<R>) = seq(other)
+infix operator fun Parser<*>.plus(other: Parser<Any?>) = seq(other)
 
 /** Returns the sequence of this parser followed by [other]. */
-infix fun <R> Parser<out R>.seq(other: Parser<out R>): Parser<List<R>> {
-  val left = if (this is SequenceParser<*>) parsers as List<Parser<R>> else listOf(this)
-  val right = if (other is SequenceParser<*>) other.parsers as List<Parser<R>> else listOf(other)
+infix fun Parser<Any?>.seq(other: Parser<Any?>): Parser<List<Any?>> {
+  val left = if (this is SequenceParser<*>) parsers else listOf(this)
+  val right = if (other is SequenceParser<*>) other.parsers else listOf(other)
   return SequenceParser(left + right)
 }
 
