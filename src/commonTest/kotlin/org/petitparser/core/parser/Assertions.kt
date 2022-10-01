dd0197409261f8b51f67857b261eccf898030b96
Lukas Renggli <renggli@gmail.com>
2022-10-01 11:13:49 +0200

    Better test coverage

--- src/commonTest/kotlin/org/petitparser/core/parser/Assertions.kt
+++ src/commonTest/kotlin/org/petitparser/core/parser/Assertions.kt
@@ -1,12 +1,26 @@
 package org.petitparser.core.parser
 
 import org.petitparser.core.context.Output
+import org.petitparser.core.context.ParseError
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 import kotlin.test.asserter
 
-internal fun <R> assertSuccess(output: Output<R>, value: R, position: Int) = when (output) {
+fun <R> assertSuccess(
+  parser: Parser<R>,
+  input: String,
+  value: R,
+  position: Int = input.length,
+) {
+  assertTrue(parser.accept(input), "$parser should accept $input")
+  val result = parser.parse(input)
+  assertSuccess(result, value, position)
+  assertFailsWith<UnsupportedOperationException> { result.message }
+}
+
+fun <R> assertSuccess(output: Output<R>, value: R, position: Int) = when (output) {
   is Output.Success -> {
     assertEquals(value, output.value, "value")
     assertEquals(position, output.position, "position")
@@ -14,30 +28,23 @@ internal fun <R> assertSuccess(output: Output<R>, value: R, position: Int) = whe
   is Output.Failure -> asserter.fail("Expected success, but got $output")
 }
 
-internal fun <R> assertSuccess(
-  parser: Parser<R>,
-  input: String,
-  value: R,
-  position: Int = input.length,
-) {
-  assertTrue(parser.accept(input), "$parser should accept $input")
-  assertSuccess(parser.parse(input), value, position)
-}
-
-internal fun <R> assertFailure(output: Output<R>, message: String, position: Int) = when (output) {
-  is Output.Success -> asserter.fail("Expected failure, but got $output")
-  is Output.Failure -> {
-    assertEquals(message, output.message, "message")
-    assertEquals(position, output.position, "position")
-  }
-}
-
-internal fun <R> assertFailure(
+fun <R> assertFailure(
   parser: Parser<R>,
   input: String,
   message: String,
   position: Int = 0,
 ) {
   assertFalse(parser.accept(input), "$parser should not accept $input")
-  assertFailure(parser.parse(input), message, position)
+  val result = parser.parse(input)
+  assertFailure(result, message, position)
+  var error = assertFailsWith<ParseError>(message) { result.value }
+  assertFailure(error.failure, message, position)
+}
+
+fun <R> assertFailure(output: Output<R>, message: String, position: Int) = when (output) {
+  is Output.Success -> asserter.fail("Expected failure, but got $output")
+  is Output.Failure -> {
+    assertEquals(message, output.message, "message")
+    assertEquals(position, output.position, "position")
+  }
 }
