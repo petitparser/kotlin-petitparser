fb0c42787b57e1956a5acf985168df4943ccaa99
Lukas Renggli <renggli@gmail.com>
2023-09-02 09:51:09 +0200

    Make the parser a functional interface.

--- src/commonMain/kotlin/org/petitparser/core/parser/Parser.kt
+++ src/commonMain/kotlin/org/petitparser/core/parser/Parser.kt
@@ -3,7 +3,8 @@ package org.petitparser.core.parser
 import org.petitparser.core.context.Input
 import org.petitparser.core.context.Output
 
-interface Parser<out R> {
+/** Functional interface of all parsers. */
+fun interface Parser<out R> {
   fun parseOn(input: Input): Output<R>
 }
 
