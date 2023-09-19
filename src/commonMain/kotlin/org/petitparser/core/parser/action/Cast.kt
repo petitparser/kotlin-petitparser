d30d77ca54ae413b067feaad37685a268e879add
Lukas Renggli <renggli@gmail.com>
2023-09-19 18:11:27 +0300

    Fix warning and update version.

--- src/commonMain/kotlin/org/petitparser/core/parser/action/Cast.kt
+++ src/commonMain/kotlin/org/petitparser/core/parser/action/Cast.kt
@@ -6,6 +6,7 @@ import org.petitparser.core.parser.Parser
 
 /** Returns a parser that casts its success result to [R]. */
 fun <T, R> Parser<T>.cast() = Parser { input ->
+  @Suppress("UNCHECKED_CAST")
   when (val result = this@cast.parseOn(input)) {
     is Output.Success -> result.success(result.value as R)
     is Output.Failure -> result
