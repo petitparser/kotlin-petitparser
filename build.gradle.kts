3e46beb08eff19d1e2346ee9ebacb97a7fd9d1a9
Lukas Renggli <renggli@gmail.com>
2025-10-06 19:20:26 +0200

    Update the project template with https://github.com/Kotlin/multiplatform-library-template.git.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,41 +1,4 @@
 plugins {
-  kotlin("multiplatform") version "2.2.20"
-  id("maven-publish")
-}
-
-group = "com.github.petitparser"
-version = "1.1.0"
-
-repositories {
-  mavenCentral()
-}
-
-kotlin {
-  applyDefaultHierarchyTemplate()
-
-  jvm()
-  js {
-    browser()
-    nodejs()
-  }
-  macosArm64()
-  macosX64()
-  linuxArm64()
-  linuxX64()
-  mingwX64()
-
-  sourceSets {
-    val commonMain by getting
-    val commonTest by getting {
-      dependencies {
-        implementation(kotlin("test"))
-      }
-    }
-    val jvmMain by getting
-    val jvmTest by getting
-    val jsMain by getting
-    val jsTest by getting
-    val nativeMain by getting
-    val nativeTest by getting
-  }
+    alias(libs.plugins.android.kotlin.multiplatform.library) apply false
+    alias(libs.plugins.kotlinMultiplatform) apply  false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,17 @@
 pluginManagement {
-  repositories {
-    mavenCentral()
-    gradlePluginPortal()
-  }
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
 
-plugins {
-  id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
 rootProject.name = "kotlin-petitparser"
+include(":library")
